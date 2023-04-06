496d23d4ec735c4049a917e7548bc5fe043008e4
MahmoudAlameen <alameen2591999@gmail.com>
2023-04-07 01:05:59 +0200

    add links to footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  phoneBox :boolean = false;
+  mailBox:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openClosePhoneBox()
+  {
+    if(this.phoneBox)
+       this.phoneBox = false;
+    else
+       this.phoneBox = true;   
+
+    console.log("hey iam in open")
+  }
+  openCloseMailBox()
+  {
+    if(this.mailBox)
+    this.mailBox = false;
+ else
+    this.mailBox = true;  
+    
   }
 
 }
