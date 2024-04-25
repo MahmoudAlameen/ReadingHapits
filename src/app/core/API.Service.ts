98c6a28c1085d9f2a16856f02ea17f6b4826f874
MahmoudAlameen <alameen2591999@gmail.com>
2024-04-25 19:55:33 +0200

    deploy --fix

--- src/app/core/API.Service.ts
+++ src/app/core/API.Service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 )
 export class APIService
 {
-    base:string= "http://ReadingHapitsAPI.somee.com/"
+    base:string= "http://readinghapitsapi.somee.com/"
     //base:string="https://localhost:7024/";
     AddUser:string=this.base+"users/addUser/";
     UserLogin=this.base+"users/login";
