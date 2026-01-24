66c3dd14ee10f6e250ae13334f99b3d2334bdb21
MahmoudAlameen <alameen2591999@gmail.com>
2026-01-24 13:48:26 +0200

    guard the user from open book untill login

--- src/app/learning-subject/learning-subject-routing.module.ts
+++ src/app/learning-subject/learning-subject-routing.module.ts
@@ -4,11 +4,12 @@ import { LearningSubjectComponent } from './learning-subject.component';
 import { BookComponent } from '../reading-room/components/book/book.component';
 import { ArticleComponent } from '../reading-room/components/article/article.component';
 import { PdfPreviewComponent } from '../shared/pdf-preview/pdf-preview.component';
+import { AuthGuard } from '../core/auth-guard.service';
 
 const routes: Routes = [{ path: '', component: LearningSubjectComponent},
-      {path: "book/:id", component: BookComponent},
-    {path: "article/:id", component: ArticleComponent},
-    {path: "preview-pdf", component: PdfPreviewComponent}
+      {path: "book/:id", component: BookComponent, canActivate: [AuthGuard]},
+    {path: "article/:id", component: ArticleComponent, canActivate: [AuthGuard]},
+    {path: "preview-pdf", component: PdfPreviewComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
